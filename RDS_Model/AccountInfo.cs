7176a17c48a89fe200fa0be7a481f5e1e7677fcc
rainbow1Fart <zhuhanlin516@gmail.com>
2019-06-26 01:21:33 +0800

    更改AccountInfo类

--- RDS_Model/AccountInfo.cs
+++ RDS_Model/AccountInfo.cs
@@ -5,7 +5,33 @@ using System.Text;
 
 namespace RDS_Model
 {
-    public class Class1
+    public class AccountInfo
     {
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Person { get; set; }
+
+        /// <summary>
+        /// 账号
+        /// </summary>
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 安全码
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 权限
+        /// </summary>
+        public int Power { get; set; }
     }
 }
